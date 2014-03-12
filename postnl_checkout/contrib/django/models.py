97b5122cada7e9cc3c4a4fff6052cbba6cde1b14
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2014-03-12 17:18:45 +0100

    Attempt using a JSONField for storing data.

--- postnl_checkout/contrib/django/models.py
+++ postnl_checkout/contrib/django/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from jsonfield import JSONField
+
 from postnl_checkout.client import PostNLCheckoutClient
 
 from .utils import SudsDjangoCache
@@ -8,6 +10,21 @@ from .settings import postnl_checkout_settings as settings
 
 class Order(models.Model):
     """ Django model representing the result of the ReadOrder call. """
+
+    # Primary identifier
+    order_token = models.CharField(max_length=255, primary=True)
+
+    # Other indexe fields
+    order_ext_ref = models.CharField(db_index=True, max_length=255)
+    order_date = models.DateField(db_index=True)
+    customer_ext_ref = models.CharField(db_index=True, max_length=255)
+
+    # Raw data
+    prepare_order_request = JSONField()
+    prepare_order_response = JSONField()
+
+    read_order_request = JSONField()
+    read_order_response = JSONField()
 
     def __init__(self, *args, **kwargs):
         """ Make a client available. """
@@ -23,3 +40,31 @@ class Order(models.Model):
             timouet=settings.TIMEOUT,
             cache=suds_cache
         )
+
+    @classmethod
+    def prepare_order(cls, params):
+        """ Call PrepareOrder and create Order using resulting token. """
+
+        # Assert required attributes
+        assert 'Order' in params
+
+        order_data = params['Order']
+
+        assert 'ExtRef' in order_data
+        assert 'OrderDatum' in order_data
+        assert 'VerzendDatum' in order_data
+
+        # Prepare order
+        response = self.client.prepare_order(params)
+
+        # Store data
+        order = cls(
+            order_ext_ref=order_data['ExtRef'],
+            order_date=order_data['OrderDatum']
+            prepare_order_request=params,
+            prepare_order_response=response
+        )
+
+        # Validate and save
+        order.clean()
+        order.save()
